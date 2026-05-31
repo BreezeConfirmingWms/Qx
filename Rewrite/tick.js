9517a07e6a7459daf38ab3940e10f8b33fac026d
BreezeConfirmingWms <wmsthinksv@gmail.com>
2026-05-31 14:08:21 +0800

    tick

---
+++ Rewrite/tick.js
@@ -0,0 +1,17 @@
+/*
+[rewrite_local]
+^https:\/\/ticktick\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/BreezeConfirmingWms/Qx/refs/heads/develop/Rewrite/ticktick.js
+[mitm] 
+hostname = ticktick.com
+*/
+let obj = JSON.parse($response.body);
+
+obj = {
+    ...obj,
+    "needSubscribe" : false,
+    "pro" : true,
+    "teamPro" : true,
+    "teamUser" : true,
+}
+
+$done({body: JSON.stringify(obj)});
