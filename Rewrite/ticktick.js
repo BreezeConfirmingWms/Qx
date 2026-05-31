6d19a72091e56cb5b1822cad6428fd13bfe4b188
BreezeConfirmingWms <wmsthinksv@gmail.com>
2026-05-31 14:02:50 +0800

    ticktick_1

--- Rewrite/ticktick.js
+++ Rewrite/ticktick.js
@@ -1,14 +1,9 @@
-/******************************
-脚本功能：TickTick-解锁高级会员
-脚本作者：yum
-注意事项：需登录
-使用声明：️仅供学习交流
-*******************************
+/*
 [rewrite_local]
-^https:\/\/ticktick\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/BreezeConfirmingWms/Qx/refs/heads/develop/Rewrite/ticktick.js
+^https:\/\/ticktick\.com\/api\/v2\/user\/status url script-response-body ticktick.js
 [mitm] 
 hostname = ticktick.com
-*******************************/
+*/
 let obj = JSON.parse($response.body);
 
 obj = {
