4944edae44d17bb7a3b1f65b7883c0d01fdd2863
BreezeConfirmingWms <wmsthinksv@gmail.com>
2026-05-31 14:03:39 +0800

    ticktick_2

--- Rewrite/ticktick.js
+++ Rewrite/ticktick.js
@@ -1,6 +1,6 @@
 /*
 [rewrite_local]
-^https:\/\/ticktick\.com\/api\/v2\/user\/status url script-response-body ticktick.js
+^https:\/\/ticktick\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/BreezeConfirmingWms/Qx/refs/heads/develop/Rewrite/ticktick.js
 [mitm] 
 hostname = ticktick.com
 */
